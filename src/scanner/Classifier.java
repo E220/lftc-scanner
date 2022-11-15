6d6834a66201d12e7cda4cf9bca0accf366ebaa5
Tiha <tgtihamer@yahoo.com>
2022-11-15 21:32:38 +0200

    refactor classifier

--- src/scanner/Classifier.java
+++ src/scanner/Classifier.java
@@ -1,6 +1,6 @@
 package scanner;
 
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Classifier {
@@ -11,27 +11,31 @@ public class Classifier {
         NUMBER_CONST,
     }
 
+    @FunctionalInterface
+    private interface Condition {
+        boolean matches(String string);
+    }
+
     private final Pattern IDENTIFIER = Pattern.compile("[a-zA-Z][a-zA-Z0-9]*");
     private final Pattern STRING_CONST = Pattern.compile("\".*\"");
     private final Pattern NUMBER_CONST = Pattern.compile("([+-]?([1-9][0-9]*)(\\.[0-9]*[1-9])?|[+-]?0\\.[0-9]*[1-9]|0)");
-    private final Set<String> tokens;
+
+    private final Map<Class, Condition> conditions;
 
     public Classifier(Set<String> tokens) {
-        this.tokens = tokens;
+        this.conditions = Collections.unmodifiableMap(new LinkedHashMap<>(){{
+            this.put(Class.TOKEN, tokens::contains);
+            this.put(Class.IDENTIFIER, string -> IDENTIFIER.matcher(string).matches());
+            this.put(Class.STRING_CONST, string -> STRING_CONST.matcher(string).matches());
+            this.put(Class.NUMBER_CONST, string -> NUMBER_CONST.matcher(string).matches());
+        }});
     }
 
     public Class classify(String string) {
-        if (tokens.contains(string)) {
-            return Class.TOKEN;
-        }
-        if (IDENTIFIER.matcher(string).matches()) {
-            return Class.IDENTIFIER;
-        }
-        if (STRING_CONST.matcher(string).matches()) {
-            return Class.STRING_CONST;
-        }
-        if (NUMBER_CONST.matcher(string).matches()) {
-            return Class.NUMBER_CONST;
+        for (final Map.Entry<Class, Condition> entry : this.conditions.entrySet()) {
+            if (entry.getValue().matches(string)) {
+                return entry.getKey();
+            }
         }
         return null;
     }
